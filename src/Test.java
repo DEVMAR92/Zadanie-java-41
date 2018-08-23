7e9ddc46bc24bbaa3446041fa2990fdad8396077
Marcin <Majsyn@gmail.com>
2018-08-23 14:16:45 +0200

    V 0.2

--- src/Test.java
+++ src/Test.java
@@ -6,31 +6,69 @@ public class Test {
         Random random = new Random();
         TreeSet<Integer> set = new TreeSet<>();
 
-        int number = 0;
-        int sum = 0;
 
         while (set.size() != 50) {
-            set.add(number += random.nextInt(1000));
-            sum += number;
-            number = 0;
-
+            set.add(random.nextInt(1000));
         }
-
-        int average = sum / set.size();
 
         System.out.println(set);
-        System.out.println("Najmniejsza liczba: " + set.first());
-        System.out.println("Największa liczba: " + set.last());
-        System.out.println("Srednia ze wszystkich liczb: " + average);
-        System.out.println("Liczby większe od średniej:");
-        int holder = set.higher(average);
-        while (set.iterator().hasNext()) {
-            System.out.print(holder + ", ");
-            if (set.higher(holder) == null) {
-                break;
-            }
-            holder = set.higher(holder);
-        }
+
+        int min = minimal(set);
+        int max = maximal(set);
+        int avg = average(set);
+
+        System.out.println("Miminalna wartość: " + min);
+        System.out.println("Maksymalna wartość: " + max);
+        System.out.println("Srednia: " + avg);
+        greatersThanAverage(set);
+
+        System.out.println(set);
+
 
     }
+
+    static int minimal(TreeSet<Integer> set) {
+
+        return set.first();
+    }
+
+    static int maximal(TreeSet<Integer> set) {
+
+        return set.last();
+    }
+
+    static int average(TreeSet<Integer> set) {
+
+        int sum = 0;
+        int sizeOfSet = set.size();
+        TreeSet<Integer> setHolder = new TreeSet<>();
+        setHolder.addAll(set);
+
+        for (int i = 0; i < sizeOfSet; i++) {
+            sum += setHolder.first();
+            setHolder.remove(setHolder.first());
+        }
+        int average = sum / sizeOfSet;
+
+        return average;
+    }
+
+    static void greatersThanAverage(TreeSet<Integer> set) {
+
+        int avarageHolder = average(set);
+        int sizeOfSet = set.size();
+        TreeSet<Integer> setHolder = new TreeSet<>();
+        setHolder.addAll(set);
+        System.out.println("Wszystkie liczby większe od sredniej " + avarageHolder + ":");
+        for (int i = 0; i < sizeOfSet; i++) {
+            if (setHolder.first() >= avarageHolder) {
+                System.out.print(setHolder.first() + " ");
+                setHolder.remove(setHolder.first());
+            } else {
+                setHolder.remove(setHolder.first());
+            }
+        }
+        System.out.println("\n");
+    }
+
 }
